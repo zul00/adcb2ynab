b9f73c50c09fa791189abcfe149f4119a9049f7e
Zulkarnaen <zul.zulkarnaen@gmail.com>
2023-01-01 21:16:30 +0400

    Started by adding a dummy code
    - Use argparse to provide path to the csv file
    - Use csv to read the file

--- convert.py
+++ convert.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
+import csv
 
 if __name__ == "__main__":
-    print("Hello there...")
+    parser = argparse.ArgumentParser(description='Parse ')
+    parser.add_argument('path', type=str, help='Path to the csv file.')
+
+    args = parser.parse_args()
+
+    with open(args.path, "r") as infile:
+        with open("./output.csv", "w") as outfile:
+            content = csv.reader(infile)
+
+            for row in content:
+                print(row)
