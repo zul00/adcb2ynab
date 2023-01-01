aea9fb4d14c5eb8ffc2816c089680b034bcf1a2b
Zulkarnaen <zul.zulkarnaen@gmail.com>
2023-01-01 21:53:46 +0400

    Uses regex to format ADCB CC statement to YNAB
    - Added gitignore
    - Added note about formatting
    - Find row with date in it
      Using regex
    - Adjust the format to YNAB

--- convert.py
+++ convert.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+'''
+Script to convert csv file to YNAB expected format
+
+"Date", "Payee", "Memo", "Outflow", "Inflow"
+'''
+
 import argparse
 import csv
+import re
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse ')
@@ -10,8 +17,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.path, "r") as infile:
+        # TODO: Change it to an argument? Or convert it from input
         with open("./output.csv", "w") as outfile:
-            content = csv.reader(infile)
+            reader = csv.reader(infile)
+            writer = csv.DictWriter(
+                outfile, ["Date", "Payee", "Memo", "Inflow", "Outflow"])
+            writer.writeheader()
 
-            for row in content:
-                print(row)
+            re_date = re.compile(r"\d{2}/\d{2}/\d{4}")
+            for row in reader:
+                for column in row:
+                    if re_date.match(column):
+                        outflow = 0
+                        inflow = 0
+                        if row[2] == "DR":
+                            outflow = row[3]
+                        elif row[2] == "CR":
+                            inflow = row[3]
+
+                        writer.writerow(
+                            {"Date": row[0], "Payee": "", "Memo": row[1],
+                             "Inflow": inflow, "Outflow": outflow})
